47325cca75664e31222d8ed8369e44d966a9f039
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-10-21 09:01:32 +0530

    "Added user routes and removed upload-book endpoint"

--- Backend/index.js
+++ Backend/index.js
@@ -6,23 +6,48 @@ import 'dotenv/config'
 
 const app = express();
 
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 5000
 
 // database connection
 connectDB();
+
+
+// Routes
+// user routes 
+app.post('/api/users/register', (req, res) => {
+    res.send('User Registered');
+})
+
+//loguin
+app.post('/api/users/login', (req, res) => {
+    res.send('User Logged in');
+})
+
+//update
+app.put('/api/users/update', (req, res) => {
+    res.send(`User update`);
+})
+
+//delete
+app.delete('/api/users/:id', (req, res) => {
+    res.send(`User deleted`);
+})
+
+// fetch users
+
+app.get('/api/users', (req, res) => {
+    res.send(`All Users`);
+})
+
+
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`app listening on ${port}`);
-    
+
 })
 
-
-app.post ('/upload-book', async(req ,res) =>{
-    const data = req.body
-    
-})
